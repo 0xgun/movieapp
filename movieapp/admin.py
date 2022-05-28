9f1d6db73f4638b8e39ead28a0dabee67a078b9b
Ganeshan M <86967803+mganeshan29@users.noreply.github.com>
2022-05-28 22:28:50 +0530

    Test (#6)
    * added layout
    
    * added movie model
    
    * pass data

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import * 
 # Register your models here.
+class MyMovie(admin.ModelAdmin):
+    list_display=('name','description','trailer','year','star','show')
+    list_filter = ('star','show')
+admin.site.register(movie,MyMovie)
