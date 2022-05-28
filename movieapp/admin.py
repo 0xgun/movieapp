d38f4c53a8a5e50bb723056608a41ab9a1c9a1c2
mganeshan29 <ganeshanmadesh36@outlook.com>
2022-05-28 22:25:02 +0530

    pass data

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import * 
 # Register your models here.
-admin.site.register(movie)
+class MyMovie(admin.ModelAdmin):
+    list_display=('name','description','trailer','year','star','show')
+    list_filter = ('star','show')
+admin.site.register(movie,MyMovie)
--- movieapp/views.py
+++ movieapp/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{})
+    # movie_data="THE GRAY MAN"
+    movie_data=[
+        {
+            "name":"THE GRAY MANWeb of Make Believe: Death, Lies and the Internet",
+            "year":"2022"
+        },
+        {
+            "name":"THE GRAY MAN",
+            "year":"2022"
+        },
+        {
+            "name":"Spriggan",
+            "year":"2022"
+        }
+    ]
+    return render(request, 'home.html',{"movie":movie_data})
 def testimonials(request):
     return render(request, 'testimonials.html',{})
